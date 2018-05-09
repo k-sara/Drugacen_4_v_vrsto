9c62403d8442949a7918cc2665596e49259a62ab
Sara Kačarević <sara.kacarevic@student.fmf.uni-lj.si>
2018-05-09 09:26:30 +0200

    Ustvarjen razred štirke in dodan izračun štirk v igri. Dodana metoda, ki preverja če so štirke enake.

--- Drugacen_4_v_vrsto/src/logika/Igra.java
+++ Drugacen_4_v_vrsto/src/logika/Igra.java
@@ -9,10 +9,41 @@ public class Igra {
 	
 	public static final int N = 6;
 	
+	/* Atributi razreda Igra. */
+	
 	private Polje[][] plosca;
 	private Igralec naPotezi;
 	
+	private static final List<Stirke> stirke = new LinkedList<Stirke>();
 	
+	/* Na zaèetku igre (ko se prviè požene igra oziroma program) se ustvari seznam vseh možnih štirk.*/
+	
+	{ /*Doloèimo možne smeri štirk*/
+		int[][] smeri = {{1,0}, {0,1}, {1,1}, {1,-1}};
+		for (int x = 0; x < N; x++) {
+			for (int y = 0; y < N; y++) {
+				for (int[] smer : smeri){
+					int dx = smer[0];
+					int dy = smer[1];
+					if ((0 <= x + (N - 1)*dx) && (0 <= y + (N -1)*dy) && (x + (N - 1)*dx < N) && (y + (N -1)*dy < N)) {
+						/* Ustvari novi tabeli, ena s 4 x in druga s 4 y. */
+						int[] stirka_x = new int[4];
+						int[] stirka_y = new int[4];
+						for (int k = 0; k < 4; k++) {
+							/* Doda elemente v tabeli. */
+							stirka_x[k] = x + dx*k;
+							stirka_y[k] = y + dy*k;
+						}
+						/* V seznam štirk, ki se ustvari na zaèetku, doda nove štirke.*/
+						stirke.add(new Stirke(stirka_x, stirka_y));
+						
+					}
+				}
+			}
+		}
+	}
+	
+	/* Ustvari novo igro s praznimi polji in doloèi, da igro zaène rdeèi. */
 	
 	public Igra() {
 		plosca = new Polje[N][N];
@@ -32,28 +63,47 @@ public class Igra {
 		if (plosca[x][y] == Polje.PRAZNO){
 			if (plosca[x+1][y] != Polje.PRAZNO || plosca[x-1][y] != Polje.PRAZNO || plosca[x][y+1] != Polje.PRAZNO || plosca[x+1][y-1] != Polje.PRAZNO){
 				return true;
-			}else {
-				return false;
 			}
 		}
+		return false;
 	}
 
-	/* 
-	 * Možne poteze
-	 */
+	/* Metoda, ki izraèuna možne poteze in vrne seznam teh. */
 
 	public List<Poteza> poteze() {
 		LinkedList<Poteza> seznampotez = new LinkedList<Poteza>();
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++){
-				if sosedi
+				if (sosedi(i,j) == true){
 				seznampotez.add(new Poteza(i,j));
+				}
+			}
+		}
 	return seznampotez;
 	}
-
-	/*
-	 * Stanje igre
-	 */
+	
+	/* Metoda, ki preverja, èe so vsa polja štirke enaka. Torej ali so vsa rdeèa oziroma modra. 
+	 * Èe niso enaka vrne false, èe pa so vsa 4 polja enaka pa true. */
+	
+	private boolean vse_enake(Stirke s, Polje p) {
+		for (int k = 0; k < 4; k++) {
+			if (plosca[s.x[k]][s.y[k]] != p) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/* Stanje igre */
+	
+	public Stanje stanje() {
+		for (Stirke s : stirke) {
+			/* Preveri èe je zmagal rdeèi */
+			if (vse_enake(s, Polje.RED)) {
+				Stanje stanje = Stanje.ZMAGA_RED;
+			}
+		}
+	}
 	
 	/*
 	 * Odigraj novo potezo
---
+++ Drugacen_4_v_vrsto/src/logika/Stirke.java
@@ -0,0 +1,19 @@
+package logika;
+
+public class Stirke {
+	
+	/* Stirke so vse možne zmagovalne kombinacije. 
+	 * Predstavljene so z dvema tabelama dolzine 4. Prva tabela predstavlja x in druga y koordinate. */
+	
+	public int[] x;
+	public int[] y;
+	
+	public Stirke(int[] x, int[] y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+
+}
