c2ca7d2ee45a1614b6ec2ad8fb972e55eddd8372
Sara Kačarević <sara.kacarevic@student.fmf.uni-lj.si>
2018-05-09 10:03:53 +0200

    Dodan paket za teste

---
+++ Drugacen_4_v_vrsto/src/test/TestLogika.java
@@ -0,0 +1,13 @@
+package test;
+
+import junit.framework.TestCase;
+import logika.Igra;
+import logika.Stanje;
+
+public class TestLogika extends TestCase {
+	
+	public void testIgra() {
+		Igra igra = new Igra();
+	}
+
+}
